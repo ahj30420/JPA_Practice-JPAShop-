1aaed57b8cc8f26214c90a10db998305833463ac
ahj30420 <ahj30420@naver.com>
2023-11-22 02:36:13 +0900

    JPA_Practice

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@AllArgsConstructor
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() {
+    }
+}
